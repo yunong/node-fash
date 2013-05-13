8f7fc995afcacccdd688f92910775c6640cb89d4
Yunong Xiao <yunong@joyent.com>
2013-05-13 10:53:06 -0700

    add missing bin folder

---
+++ bin/fash.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright (c) 2013, Yunong J Xiao. All rights reserved.
+ *
+ * fash.js: CLI tool for node-fash
+ */
+
+var bunyan = require('bunyan');
+var cmdln = require('cmdln');
+var fash = require('../lib/index');
+var util = require('util');
+
+var Cmdln = cmdln.Cmdln;
+
+function Fash() {
+    Cmdln.call(this, {
+        name: 'fash',
+        desc: 'fash cmdline tool',
+        // Custom options. By default you get -h/--help.
+        options: [
+            {names: ['help', 'h'], type: 'bool',
+                help: 'Print help and exit.'},
+                {name: 'version', type: 'bool',
+                    help: 'Print version and exit.'}
+        ]
+    });
+
+    this.log = new bunyan({
+        name: 'fash',
+        level: process.env.LOG_LEVEL || 'warn'
+    });
+}
+util.inherits(Fash, Cmdln);
+
+Fash.prototype.do_create = function(subcmd, opts, args, callback) {
+    var self = this;
+
+    if (opts.help) {
+        this.do_help('help', {}, [subcmd], callback);
+        return (callback());
+    }
+
+    if (args.length !== 0 || !opts.v || !opts.p) {
+        this.do_help('help', {}, [subcmd], callback);
+        return (callback());
+    }
+
+    var pnodes = opts.p.split(' ');
+    var chash = fash.create({
+        log: self.log,
+        algorithm: fash.ALGORITHMS.SHA256,
+        pnodes: pnodes,
+        vnodes: opts.v
+    });
+
+    console.log(chash.serialize());
+
+    return (callback());
+};
+Fash.prototype.do_create.options = [{
+    names: [ 'v', 'vnode' ],
+    type: 'number',
+    help: 'number of vnodes'
+}, {
+    names: [ 'p', 'pnode' ],
+    type: 'string',
+    help: 'physical node names'
+}];
+Fash.prototype.do_create.help = (
+    'create a consistent hash ring.\n'
+    + '\n'
+    + 'usage:\n'
+    + '     fash create [options] \n'
+    + '\n'
+    + '{{options}}'
+);
+
+cmdln.main(Fash); // mainline
